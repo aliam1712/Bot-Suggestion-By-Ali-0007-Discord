6d7ca8860d89565e2cbca471d992f971b91b0da8
Ali <67750450+aliam1712@users.noreply.github.com>
2021-05-16 21:14:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,21 +14,6 @@ client.on('ready', () =>{
 
 
 
-//By: Ali#0007
-client.on('message', message =>{
-    if(message.content === prefix + 'help'){
-        const embed = new Discord.MessageEmbed()
-        .setTitle(`Help`)
-        .setDescription(`
-        1) ${prefix}sugs on / ${prefix}sugs off | turn on and off the suggestions.
-        2) ${prefix}set-room | set the suggestion room.
-        3) ${prefix}sugs-status | show the suggestion status.
-        `)//By: Ali#0007
-        .setFooter(message.author.tag, message.author.avatarURL())
-        message.channel.send(embed)
-    }
-
-})
 
 client.on('message', async message =>{
     if(message.content.startsWith(prefix + 'sugs on')){//By: Ali#0007
@@ -48,23 +33,33 @@ client.on('message', async message =>{
         let ChannelFetch = await db.fetch(`Channel_${message.guild.id}`)
         let sugsStatus = await db.fetch(`Sugs_${message.guild.id}`)
         await message.channel.send(`Sugs is: **${sugsStatus}**\nThe Room is: <#${ChannelFetch}>`)//By: Ali#0007
+    }else {
+        if(message.content === prefix + 'help'){
+            const embed = new Discord.MessageEmbed()
+            .setTitle(`Help`)
+            .setDescription(`
+            1) ${prefix}sugs on / ${prefix}sugs off | turn on and off the suggestions.
+            2) ${prefix}set-room | set the suggestion room.
+            3) ${prefix}sug-status | show the suggestion status.
+            `)//By: Ali#0007
+            .setFooter(message.author.tag, message.author.avatarURL())
+            message.channel.send(embed)
+        } else {
+            if(message.content.startsWith(prefix + 'set-room')){//By: Ali#0007
+                let Suggestion = await db.fetch(`Sugs_${message.guild.id}`)
+                if (Suggestion == "off" || Suggestion == null) return message.channel.send(`Please activate the sugs first ${prefix}sugs on`)
+                if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You dont have the required permission `ADMINISTRATOR` ");
+                let ChannelID = message.content.split(" ").slice(1).join(" ");
+                if(!ChannelID) return message.channel.send(`Please specify an id for room`)
+        
+                await db.set(`Channel_${message.guild.id}`, ChannelID)//By: Ali#0007
+                message.channel.send(`Done set the room <#${ChannelID}>`)
+            }
+        }
     }
     }})//By: Ali#0007
 
 
-client.on('message', async message =>{
-    if(message.content.startsWith(prefix + 'set-room')){//By: Ali#0007
-        let Suggestion = await db.fetch(`Sugs_${message.guild.id}`)
-        if (Suggestion == "off") return message.channel.send(`Please activate the sugs first ${prefix}sugs on`)
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You dont have the required permission `ADMINISTRATOR` ");
-        let ChannelID = message.content.split(" ").slice(1).join(" ");
-        if(!ChannelID) return message.channel.send(`Please specify a room`)
-
-        await db.set(`Channel_${message.guild.id}`, ChannelID)//By: Ali#0007
-        message.channel.send(`Done set the room <#${ChannelID}>`)
-    }
-    //By: Ali#0007
-})
 
 
 client.on('message', async message =>{//By: Ali#0007
