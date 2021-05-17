b595cfe651f6f6c4709f7d83cf4d0ec63c2a9728
Ali <67750450+aliam1712@users.noreply.github.com>
2021-05-17 21:10:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+  * Code By: Ali#0007
+  * Please Follow Me On Github
+*/
 const Discord = require('discord.js');
 const client = new Discord.Client()
 const config = require('./config.json')///config file
@@ -6,7 +10,7 @@ const db = require('quick.db');////do 'npm install quick.db'
 let prefix = config.prefix
 
 
-//By: Ali#0007
+
 client.on('ready', () =>{
     console.log(`Logged in as ${client.user.tag}`)
     console.log(`Bot developer: Ali#0007`)
@@ -17,34 +21,34 @@ client.on('ready', () =>{
 
 
 client.on('message', async message => {
-    if (message.content.startsWith(prefix + 'sugs on')) { //By: Ali#0007
+    if (message.content.startsWith(prefix + 'sugs on')) { 
         if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You dont have the required permission `ADMINISTRATOR` ");
 
         await db.set(`Sugs_${message.guild.id}`, "on")
-        message.channel.send(`Suggestion turned on! `) //By: Ali#0007
+        message.channel.send(`Suggestion turned on! `) 
     } else {
-        if (message.content.startsWith(prefix + 'sugs off')) { //By: Ali#0007
+        if (message.content.startsWith(prefix + 'sugs off')) { 
             if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You dont have the required permission `ADMINISTRATOR` ");
 
             await db.set(`Sugs_${message.guild.id}`, "off")
-            message.channel.send(`Suggestion turned off! `) //By: Ali#0007
+            message.channel.send(`Suggestion turned off! `) 
 
         } else {
             if (message.content.startsWith(prefix + 'sug-status')) {
                 if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You dont have the required permission `ADMINISTRATOR` ");
                 let ChannelFetch = await db.fetch(`Channel_${message.guild.id}`)
                 let sugsStatus = await db.fetch(`Sugs_${message.guild.id}`)
-                await message.channel.send(`Sugs is: **${sugsStatus}**\nThe Room is: <#${ChannelFetch}>`) //By: Ali#0007
+                await message.channel.send(`Sugs is: **${sugsStatus}**\nThe Room is: <#${ChannelFetch}>`) 
 
             } else {
-                if (message.content.startsWith(prefix + 'set-room')) { //By: Ali#0007
+                if (message.content.startsWith(prefix + 'set-room')) { 
                     let Suggestion = await db.fetch(`Sugs_${message.guild.id}`)
                     if (Suggestion == "off" || Suggestion == null) return message.channel.send(`Please activate the sugs first ${prefix}sugs on`)
                     if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You dont have the required permission `ADMINISTRATOR` ");
                     let ChannelID = message.content.split(" ").slice(1).join(" ");
                     if (!ChannelID) return message.channel.send(`Please specify an id for room`)
 
-                    await db.set(`Channel_${message.guild.id}`, ChannelID) //By: Ali#0007
+                    await db.set(`Channel_${message.guild.id}`, ChannelID) 
                     message.channel.send(`Done set the room <#${ChannelID}>`)
                 } else {
                     if (message.content === prefix + 'help') {
@@ -54,15 +58,15 @@ client.on('message', async message => {
                                 1) ${prefix}sugs on / ${prefix}sugs off | turn on and off the suggestions.
                                 2) ${prefix}set-room | set the suggestion room.
                                 3) ${prefix}sug-status | show the suggestion status.
-            `) //By: Ali#0007
+            `) 
                             .setFooter(`Request By: ${message.author.tag}`, message.author.avatarURL())
                         message.channel.send(embed)
                     } else {
-                        { //By: Ali#0007
+                        { 
                             let suggestion = await db.fetch(`Sugs_${message.guild.id}`)
                             if (suggestion == "off") {
                                 return;
-                            } else if (suggestion == "on") { //By: Ali#0007
+                            } else if (suggestion == "on") { 
                                 if (!message.guild.id) return;
                                 if (message.author.bot) return;
                                 let sug = message.content;
@@ -70,7 +74,7 @@ client.on('message', async message => {
 
                                 if (message.channel.id !== ChannelSug) return;
 
-                                const SugEmbed = new Discord.MessageEmbed() //By: Ali#0007
+                                const SugEmbed = new Discord.MessageEmbed() 
                                     .setAuthor(message.author.tag, message.author.avatarURL())
                                     .setTitle(`Sug is:`)
                                     .setDescription(`${sug}`)
@@ -79,7 +83,7 @@ client.on('message', async message => {
                                 message.channel.send(SugEmbed).then(async message => {
                                     await message.react('✅')
                                     await message.react('❌')
-                                }) //By: Ali#0007
+                                }) 
                             }
 
 
@@ -90,5 +94,5 @@ client.on('message', async message => {
             }
         }
     }
-}) //By: Ali#0007
+}) 
 
